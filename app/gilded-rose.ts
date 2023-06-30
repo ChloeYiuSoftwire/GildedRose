c41074cffc58f511208601fa302b1ab91fb37f42
ChloeYiuSoftwire <chloe.yiu@softwire.com>
2023-06-30 13:51:39 +0100

    Unit test for updating quality

--- app/gilded-rose.ts
+++ app/gilded-rose.ts
@@ -21,14 +21,14 @@ export class GildedRose { // an array of items
     for (let i = 0; i < this.items.length; i++) {
         const name = this.items[i].name
         const sellIn = this.items[i].sellIn;
-        const quality = this.items[i].quality;
+        const quality = this.items[i].quality; 
 
       if (
         this.items[i].name != "Aged Brie" &&
-        this.items[i].name != "Backstage passes to a TAFKAL80ETC concert"
+        this.items[i].name != "Backstage"
       ) {
         if (this.items[i].quality > 0) {
-          if (this.items[i].name != "Sulfuras, Hand of Ragnaros") {
+          if (this.items[i].name != "Sulfuras") {
             this.items[i].quality = this.items[i].quality - 1;
           }
         }
@@ -36,7 +36,7 @@ export class GildedRose { // an array of items
         if (this.items[i].quality < 50) {
           this.items[i].quality = this.items[i].quality + 1;
           if (
-            this.items[i].name == "Backstage passes to a TAFKAL80ETC concert"
+            this.items[i].name == "Backstage"
           ) {
             if (this.items[i].sellIn < 11) {
               if (this.items[i].quality < 50) {
@@ -53,16 +53,16 @@ export class GildedRose { // an array of items
       }
 
       // update sellIn values 
-      this.items[i].sellIn = (name == "Sulfuras, Hand of Ragnaros") ? sellIn : sellIn - 1;
+      this.items[i].sellIn = (name == "Sulfuras") ? sellIn : sellIn - 1;
 
       // update quality  
       if (this.items[i].sellIn < 0) {
         if (this.items[i].name != "Aged Brie") {
           if (
-            this.items[i].name != "Backstage passes to a TAFKAL80ETC concert"
+            this.items[i].name != "Backstage"
           ) {
             if (this.items[i].quality > 0) {
-              if (this.items[i].name != "Sulfuras, Hand of Ragnaros") {
+              if (this.items[i].name != "Sulfuras") {
                 this.items[i].quality = this.items[i].quality - 1;
               }
             }
--- test/gilded-rose.spec.ts
+++ test/gilded-rose.spec.ts
@@ -12,16 +12,14 @@ describe("Gilded Rose", function () {
 
   describe("Update SellIn", function () {
     it("Sulfuras items should not have SellIn updated", function () {
-      const gildedRose = new GildedRose([
-        new Item("Sulfuras, Hand of Ragnaros", 0, 0),
-      ]);
+      const gildedRose = new GildedRose([new Item("Sulfuras", 0, 0)]);
       const initialSellIn = gildedRose.items[0].sellIn;
       gildedRose.updateQuality();
       const finalSellIn = gildedRose.items[0].sellIn;
       expect(finalSellIn - initialSellIn).to.equal(0);
     });
 
-    it("Non Sulfuras items should have SellIn updated", function () {
+    it("Other items should have SellIn updated", function () {
       const gildedRose = new GildedRose([new Item("foo", 0, 0)]);
       const initialSellIn = gildedRose.items[0].sellIn;
       gildedRose.updateQuality();
@@ -31,30 +29,65 @@ describe("Gilded Rose", function () {
   });
 
   describe("Update Quality", function () {
-    it("Sulfuras items should not have Quality degraded", function () {
-      const gildedRose = new GildedRose([
-        new Item("Sulfuras, Hand of Ragnaros", 5, 5),
-      ]);
-      const initialQuality = gildedRose.items[0].quality;
-      gildedRose.updateQuality();
-      const finalQuality = gildedRose.items[0].quality;
-      expect(finalQuality - initialQuality).to.equal(0);
+    describe("Sulfuras", function () {
+      it("Sulfuras items should not have Quality degraded", function () {
+        const gildedRose = new GildedRose([new Item("Sulfuras", 5, 5)]);
+        const initialQuality = gildedRose.items[0].quality;
+        gildedRose.updateQuality();
+        const finalQuality = gildedRose.items[0].quality;
+        expect(finalQuality - initialQuality).to.equal(0);
+      });
     });
 
-    it("Normal items that didn't pass sellInDate should have Quality degraded by 1", function () {
-      const gildedRose = new GildedRose([new Item("foo", 5, 5)]);
-      const initialQuality = gildedRose.items[0].quality;
-      gildedRose.updateQuality();
-      const finalQuality = gildedRose.items[0].quality;
-      expect(finalQuality - initialQuality).to.equal(-1);
+    describe("Normal items", function () {
+      it("Normal items that didn't pass sellInDate should have Quality degraded by 1", function () {
+        const gildedRose = new GildedRose([new Item("foo", 5, 5)]);
+        const initialQuality = gildedRose.items[0].quality;
+        gildedRose.updateQuality();
+        const finalQuality = gildedRose.items[0].quality;
+        expect(finalQuality - initialQuality).to.equal(-1);
+      });
+
+      it("Normal items that passed sellInDate should have Quality degraded by 2", function () {
+        const gildedRose = new GildedRose([new Item("foo", 0, 5)]);
+        const initialQuality = gildedRose.items[0].quality;
+        gildedRose.updateQuality();
+        const finalQuality = gildedRose.items[0].quality;
+        expect(finalQuality - initialQuality).to.equal(-2);
+      });
     });
 
-    it("Normal items that passed sellInDate should have Quality degraded by 2", function () {
-      const gildedRose = new GildedRose([new Item("foo", 0, 5)]);
-      const initialQuality = gildedRose.items[0].quality;
-      gildedRose.updateQuality();
-      const finalQuality = gildedRose.items[0].quality;
-      expect(finalQuality - initialQuality).to.equal(-2);
+    describe("Backstage amd Aged Brie", function () {
+      it("Backstage amd Aged Brie - quality increases by 1 when 10 < SellIn", function () {
+        const gildedRose = new GildedRose([new Item("Backstage", 11, 5)]);
+        const initialQuality = gildedRose.items[0].quality;
+        gildedRose.updateQuality();
+        const finalQuality = gildedRose.items[0].quality;
+        expect(finalQuality - initialQuality).to.equal(1);
+      });
+
+      it("Backstage amd Aged Brie - quality increases by 2 when 5 < SellIn <= 10", function () {
+        const gildedRose = new GildedRose([new Item("Backstage", 7, 5)]);
+        const initialQuality = gildedRose.items[0].quality;
+        gildedRose.updateQuality();
+        const finalQuality = gildedRose.items[0].quality;
+        expect(finalQuality - initialQuality).to.equal(2);
+      });
+
+      it("Backstage amd Aged Brie - quality increases by 3 when 0 < SellIn <= 5", function () {
+        const gildedRose = new GildedRose([new Item("Backstage", 1, 5)]);
+        const initialQuality = gildedRose.items[0].quality;
+        gildedRose.updateQuality();
+        const finalQuality = gildedRose.items[0].quality;
+        expect(finalQuality - initialQuality).to.equal(3);
+      });
+
+      it("Backstage amd Aged Brie - quality drops to 0 when SellIn <= 0", function () {
+        const gildedRose = new GildedRose([new Item("Backstage", 0, 5)]);
+        gildedRose.updateQuality();
+        const finalQuality = gildedRose.items[0].quality;
+        expect(finalQuality).to.equal(0);
+      });
     });
   });
 });
